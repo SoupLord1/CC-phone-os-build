ea72862490a4adc886d5ee5102ecfbbff5283c7f
SoupLord1 <113377594+SoupLord1@users.noreply.github.com>
2024-04-08 16:02:36 -0600

    Made progress on the navbar widget

--- CC phone os/startup.lua
+++ CC phone os/startup.lua
@@ -1,7 +1,26 @@
 wb = require("window_builder")
 
-screen = {}
-screen["height"], screen["width"] = term.getSize()
 
-print(screen["height"])
-print(screen["width"])
+
+screen = {}
+screen["width"], screen["height"] = term.getSize()
+
+mobile_device = false
+
+if screen["width"] == 26 then
+    mobile_device = true
+end
+
+term.clear()
+
+
+
+term.setCursorPos(1, 1)
+
+wb.widgets.navbar(1, 4, colors.red)
+
+wb.widgets.navbar(8, 5, colors.blue)
+
+while true do
+    sleep(0.01)
+end
--- CC phone os/window_builder.lua
+++ CC phone os/window_builder.lua
@@ -15,8 +15,16 @@ function window_builder.drawer(window)
 	return "" --draws the window to screen
 end
 
-function window_builder.widgets.navbar()
-	return "" -- return a navbar
+function window_builder.widgets.navbar(y, height, color)
+	i = 1
+while i < screen["width"] + 1 do
+    term.setBackgroundColor(color)
+	for j = height - 1, 0, -1 do
+		term.setCursorPos(i, y+j)
+		term.write(" ")
+    end
+    i = i + 1
+end
 end
 
 -- return the table to be used a as module in another file
