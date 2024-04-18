3f05fd39b2eeafcaaa19c9505254984d57a1f49b
SoupLord1 <113377594+SoupLord1@users.noreply.github.com>
2024-04-18 11:19:06 -0600

    sdf

--- setup.lua
+++ setup.lua
@@ -1,24 +1,9 @@
-print("Building Directories")
--- Modules
-fs.makeDir("os/modules/main_os/")
-fs.makeDir("os/modules/sha2/")
--- Default Apps
-fs.makeDir("os/default_apps/apps/")
-fs.makeDir("os/default_apps/icons/")
-fs.makeDir("os/default_apps/setup/")
-fs.makeDir("os/default_apps/storage/settings")
-
-print("Downloading Files")
-
-shell.run("pastebin get jPdJqDXV os/basalt.lua")
-
-shell.run("pastebin get P04NNdK5 os/os_startup.lua")
-
-shell.run("pastebin get HG3r2CWQ os/modules/main_os/app_loader.lua")
-
-shell.run("pastebin get xvYAg6UL os/modules/sha2/sha2.lua")
-
-shell.run("pastebin get rwVdZa5w os/default_apps/apps/settings.lua")
-shell.run("pastebin get pUpKn7hC os/default_apps/icons/settings.bimg")
-shell.run("pastebin get uqLfAMKh os/default_apps/setup/settings.app")
-shell.run("pastebin get 8UCBMTe3 os/default_apps/storage/settings/password.txt")
+print("Downloading Downloader...")
+shell.run("pastebin get sz2x1jZC clone.min.lua")
+print("Downloader Downloaded!")
+shell.run("clone.min https://github.com/SoupLord1/CC-phone-os-build.git os")
+shell.run("delete clone.min.lua")
+shell.run("cd os")
+shell.run("move startup.lua /")
+shell.run("os_startup")
+ 
