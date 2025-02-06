a93401b2437a89265a518d068d7543e7fbbbbd63
SoupLord1 <lukeluvscatsinacan@gmail.com>
2025-02-06 11:20:58 -0800

    Update os_startup.lua

--- os_startup.lua
+++ os_startup.lua
@@ -58,6 +58,7 @@ local app_fetcher = require("system.modules.custom.app_fetcher")
 local function start_app(config)
     local app = app_fetcher.fetch(config)
     local app_handle = app()(desktop)
+    basalt.debug(app_handle)
     return app_handle
 end
 
